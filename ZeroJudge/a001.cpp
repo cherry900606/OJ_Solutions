5b13935cca0f66a2993a1715077106888ad1a06b
chenchen <cherry900606@gmail.com>
2021-06-18 11:56:54 +0800

    nothing

--- ZeroJudge/a001.cpp
+++ ZeroJudge/a001.cpp
@@ -1,10 +1,1 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-string s;
- while(cin >> s){
-cout << "hello, "<< s << endl;
- }
- return 0;
-}
+#include <iostream>using namespace std;int main() {string s; while(cin >> s){cout << "hello, "<< s << endl; } return 0;}
--- ZeroJudge/a002.cpp
+++ ZeroJudge/a002.cpp
@@ -1,10 +1,1 @@
-#include <iostream>
-using namespace std;
-
-int main ()
-{
-    int a,b;
-    while( cin >> a>>b)
-        cout << a+b << endl;
-    return 0;
-}
+#include <iostream>using namespace std;int main (){    int a,b;    while( cin >> a>>b)        cout << a+b << endl;    return 0;}
